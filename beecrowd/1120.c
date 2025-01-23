35e809aea58a21facbe5e649e85baabcfa0569ca
Rodrigo Lopes Marques <102917955+RodrigoLMarques@users.noreply.github.com>
2025-01-22 23:04:14 -0300

    1120 beecrowd

---
+++ beecrowd/1120.c
@@ -0,0 +1,50 @@
+/* 
+  Nome: Rodrigo Lopes Marques
+  Matrícula: 180385
+  Exercício: 1120 - Revisão de Contrato
+  Observações: Usando Heap Max
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 10100
+
+int main() {
+  char digit, strNumber[MAX];
+  
+  while (1) {
+    scanf("%c %s", &digit, strNumber);
+    getchar();
+
+    if (digit == '0' && strcmp(strNumber, "0") == 0) {
+      break;
+    }
+    
+    char result[MAX];
+    int index = 0;
+    for (size_t i = 0; i < strlen(strNumber); i++) {
+      if (strNumber[i] != digit) {
+        result[index++] = strNumber[i];
+      }
+    }
+    result[index] = '\0';
+
+    if (index == 0) {
+      printf("0\n");
+    } else {
+      int i = 0;
+      while (result[i] == '0') {
+        i++;
+      }
+
+      if (result[i] == '\0') {
+        printf("0\n");
+      } else {
+        printf("%s\n", &result[i]);
+      }
+    }
+  }
+
+  return 0;
+}
