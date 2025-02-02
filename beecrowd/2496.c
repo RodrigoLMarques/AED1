d27b95f800a98e64bb2fcce6f84e55b03bad9703
Rodrigo Lopes Marques <102917955+RodrigoLMarques@users.noreply.github.com>
2025-02-02 20:12:24 -0300

    2496 beecrowd

--- beecrowd/2496.c
+++ beecrowd/2496.c
@@ -1,101 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX 30
+typedef struct {
+  int priority;
+  char type;
+} Problem;
 
-typedef struct TList {
-  char str[MAX];
-  int changes;
-  struct TList *next;
-} List;
-
-void sort_string(char *str);
-int count_changes(char* str, char* sorted_str, int size);
-List* insert(char* str, int changes, List *last);
-void print_list(List *list);
-void free_list(List* list);
+int compare_problems(Problem *a, Problem *b);
+void bubble_sort(Problem *array, int size);
+void read_problems(Problem *problems, int rows, int cols);
+void print_problems(Problem *problems, int size);
 
 int main() {
-  List* head = (List*) malloc(sizeof(List));
-  head->next = NULL;
-  List* last = head;
+  int rows, cols;
+  scanf("%d %d", &rows, &cols);
 
-  int n, m;
-  char str[MAX], message[MAX];
+  int total = rows * cols;
+  Problem *problems = malloc(total * sizeof(Problem));
 
-  scanf("%d", &n);
+  read_problems(problems, rows, cols);
+  bubble_sort(problems, total);
+  print_problems(problems, total);
 
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &m);
-    getchar();
-    scanf("%s", str);
-
-    char sorted_str[MAX];
-    strcpy(sorted_str, str);
-    sort_string(sorted_str);
-
-
-    int changes = count_changes(str, sorted_str, m);
-
-    last = insert(str, changes, last);
-  }
-
-  print_list(head);
-  free_list(head);
-
+  free(problems);
   return 0;
 }
 
-List* insert(char* str, int changes, List *last) {
-  List *new_cell;
-  new_cell = (List*) malloc(sizeof(List));
-  strcpy(new_cell->str, str);
-  new_cell->changes = changes;
-  new_cell->next = NULL;
-  last->next = new_cell;
-  return new_cell;
+void read_problems(Problem *problems, int rows, int cols) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      char input[3];
+      scanf("%s", input);
+      problems[i * cols + j].priority = input[0] - '0';
+      problems[i * cols + j].type = input[1];
+    }
+  }
 }
 
+void print_problems(Problem *problems, int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d%c\n", problems[i].priority, problems[i].type);
+  }
+}
 
-void sort_string(char *str) {
-  int len = strlen(str);
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = 0; j < len - i - 1; j++) {
-      if (str[j] > str[j + 1]) {
-        char temp = str[j];
-        str[j] = str[j + 1];
-        str[j + 1] = temp;
+int compare_problems(Problem *a, Problem *b) {
+  if (a->type == 'V' && b->type == 'D') {
+    return -1;
+  }
+  if (a->type == 'D' && b->type == 'V') {
+    return 1;
+  }
+  return b->priority - a->priority;
+}
+
+void bubble_sort(Problem *array, int size) {
+  for (int i = 0; i < size - 1; i++) {
+    for (int j = 0; j < size - i - 1; j++) {
+      if (compare_problems(&array[j], &array[j + 1]) > 0) {
+        Problem temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
       }
     }
-  }
-}
-
-int count_changes(char* str, char* sorted_str, int size) {
-  int changes = 0;
-  for (int i = 0; i < size; i++) {
-    if (str[i] != sorted_str[i])
-      changes++;
-  }
-
-  return changes;
-}
-
-void print_list(List *list) {
-  for (List *p = list->next; p != NULL; p = p->next) {
-    if (p->changes <= 2) {
-      printf("There are the chance.\n");
-    } else {
-      printf("There aren't the chance.\n");
-    }
-  }
-}
-
-void free_list(List* list) {
-  List* current = list;
-  while (current != NULL) {
-    List* temp = current;
-    current = current->next;
-    free(temp);
   }
 }
